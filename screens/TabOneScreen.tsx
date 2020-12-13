31b13df39383d22f8716a1e0da9cff36386dbcdf
chrisdigitallabs <chris@digital-labs.co.uk>
2020-12-13 13:31:33 +0000

    Update to logo assests

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,27 +1,48 @@
 import * as React from "react";
 import { useState } from "react";
-import { StyleSheet, TextInput } from "react-native";
+import { Button, StyleSheet, TextInput } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 
 export default function TabOneScreen() {
   const [text, setText] = useState("");
+  const onPressLearnMore = () => {
+    console.log("Learn more!");
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+      <Text style={styles.title}>Jean</Text>
+
+      <Text>Username</Text>
       <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          minWidth: 200,
+        }}
         onChangeText={(value) => setText(value)}
         value={text}
       />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text>Password</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          minWidth: 200,
+        }}
+        onChangeText={(value) => setText(value)}
+        value={text}
+      />
+      <Button
+        onPress={onPressLearnMore}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
